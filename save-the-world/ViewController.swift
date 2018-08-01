f55c6836016541a7ee33ac0b28e12499e60fff04
Walker Trent <wtrent1@walkers-mbp>
2018-07-31 20:31:52 -0500

    Initial Commit

---
+++ save-the-world/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  save-the-world
+//
+//  Created by Walker Trent on 7/31/18.
+//  Copyright © 2018 Walker Trent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
